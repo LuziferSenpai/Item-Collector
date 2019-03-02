87029a3d420d6d3ce0a0061f3f1518393424b681
Luzifer <mrcookiesmedia@gmail.com>
2019-03-02 12:00:23 +0100

    Update 0.1.2

--- control.lua
+++ control.lua
@@ -1,62 +1,45 @@
 local radius = 25
-
-
-local function ONLOAD()
-    global.itemCollectors = global.itemCollectors or {}
-end
+local de = defines.events
 
 local function ONBUILD( event )
-    local entity = event.created_entity
-    if entity.name == "item-collector-area" then
-        local surface = entity.surface
-        local force = entity.force
-        newCollector = surface.create_entity{ name = "item-collector", position = entity.position, force = force }
-        entity.destroy()
-        table.insert( global.itemCollectors, newCollector )
+    local e = event.created_entity
+    if e.name == "item-collector-area" then
+        local s = e.surface
+        local f = e.force
+        n = s.create_entity{ name = "item-collector", position = e.position, force = f }
+        e.destroy()
+        table.insert( global.itemCollectors, n )
     end
 end
 
-local function ONREMOVE( event )
-    local entity = event.entity
-    if entity.name == "item-collector" then
-        for index, l in pairs( global.itemCollectors ) do
-            if entity == l then
-                global.itemCollectors[index] = nil
-                break
-            end
-        end
-    end
-end
+script.on_init( function()
+    global.itemCollectors = global.itemCollectors or {}
+end )
 
-script.on_init( ONLOAD )
-
-script.on_event( defines.events.on_tick, function( event )
+script.on_event( de.on_tick, function( event )
     if #global.itemCollectors > 0 and event.tick % ( game.speed * 60 ) == 0 then
-        local items
-        local inventory
-        for k, collector in pairs( global.itemCollectors ) do
-            if collector.valid then
-                local posi = collector.position
-                items = collector.surface.find_entities_filtered{ area = { { posi.x - radius, posi.y - radius }, { posi.x + radius, posi.y + radius } }, name = "item-on-ground" }
-                if #items > 0 then
-                    inventory = collector.get_inventory( defines.inventory.chest )
-                    for _, item in pairs( items ) do
+        local it
+        local i
+        for k, c in pairs( global.itemCollectors ) do
+            if c.valid then
+                local posi = c.position
+                it = c.surface.find_entities_filtered{ area = { { posi.x - radius, posi.y - radius }, { posi.x + radius, posi.y + radius } }, name = "item-on-ground" }
+                if #it > 0 then
+                    i = c.get_inventory( defines.inventory.chest )
+                    for _, item in pairs( it ) do
                         local stack = item.stack
-                        if inventory.can_insert( stack ) then
-                            inventory.insert( stack )
+                        if i.can_insert( stack ) then
+                            i.insert( stack )
                             item.destroy()
                         end
                     end
                 end
             else
-                table.remove( global.itemCollectors, k )
+                global.itemCollectors[k] = global.itemCollectors[#global.itemCollectors]
+                global.itemCollectors[#global.itemCollectors] = nil
             end
         end
     end
 end )
 
-script.on_event( defines.events.on_built_entity, ONBUILD )
-script.on_event( defines.events.on_robot_built_entity, ONBUILD )
-script.on_event( defines.events.on_pre_player_mined_item, ONREMOVE )
-script.on_event( defines.events.on_robot_pre_mined, ONREMOVE )
-script.on_event( defines.events.on_entity_died, ONREMOVE )
+script.on_event( { de.on_built_entity,de.on_robot_built_entity }, ONBUILD )
